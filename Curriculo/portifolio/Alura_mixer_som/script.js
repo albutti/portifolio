e4c5f8f6c8e7a03754d4369f521d2c349c2d02b1
albutti <caique.albutti@hotmail.com>
2022-06-15 21:19:45 -0300

    atualizado

---
+++ Curriculo/portifolio/Alura_mixer_som/script.js
@@ -0,0 +1,47 @@
+var pom = document.querySelector('#som_tecla_pom');
+var clap = document.querySelector('#som_tecla_clap');
+var tim = document.querySelector('#som_tecla_tim'); 
+
+var puff = document.querySelector('#som_tecla_puff');
+var splash = document.querySelector('#som_tecla_splash');
+var toim = document.querySelector('#som_tecla_toim');
+
+var psh = document.querySelector('#som_tecla_psh');
+var tic = document.querySelector('#som_tecla_tic');
+var tom = document.querySelector('#som_tecla_tom');
+
+function tocar_tom() {
+    tom.play()
+}
+
+function tocar_tic() {
+    tic.play()
+}
+
+function tocar_psh() {
+    psh.play()
+}
+
+function tocar_toim() {
+    toim.play()
+}
+
+function tocar_splash() {
+    splash.play()
+}
+
+function tocar_puff() {
+    puff.play()
+}
+
+function tocar_tim() {
+    tim.play()
+}
+
+function tocar_clap() {
+    clap.play()
+}
+
+function tocar_pom() {
+    pom.play()
+}
